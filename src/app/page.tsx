815ece0e0e7604bdcd5b5e56240a9e49c424c3a8
Justine Dor <118171057+Jusss10@users.noreply.github.com>
2024-06-17 20:32:32 +0200

    Chnaged the header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,10 @@ export default function Home() {
 	return (
 		<main className="flex justify-center items-center scrollbar-hide ">
 			<div className="w-full scrollbar-hide">
+				<div className="flex flex-col items-center justify-center h-full">
+					<p className="text-[8vw] font-tiny5">Portfolio</p>
+					<p className="text-[7.5vw] font-tiny5">Justine Dor</p>
+				</div>
 				<Hero />
 				<About />
 				<Hero />
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -33,9 +33,8 @@ const Hero = () => {
 			<RadialGradient containerRef={container} />
 			<div className="absolute left-0 w-full h-full">
 				<div ref={container} className="h-[150vh] pb-20 pt-26 relative">
-					<p className=""> PORTFOLIO JUSTINE DOR</p>
-					<Slide direction="left" left="-20%" progress={scrollYProgress} />
-					<Slide direction="right" left="-50%" progress={scrollYProgress} />
+					<Slide direction="left" left="-100%" progress={scrollYProgress} />
+					<Slide direction="right" left="-20%" progress={scrollYProgress} />
 					<Slide direction="left" left="-75%" progress={scrollYProgress} />
 				</div>
 			</div>
